65ea8a1b1a89da132d4c0ad38ad73101f4953be6
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-05-04 15:14:06 -0300

    Added tests for shared passwords.

---
+++ ObligatorioDA1/ObligatorioDA1Tests/SharedPasswordsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainTests
+{
+    [TestClass]
+    public class SharedPasswordsTests
+    {
+        public SharedPasswordsTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void SharedPasswordIsActuallySharedTest()
+        {
+            
+        }
+
+        [TestMethod]
+        public void SharedPasswordHasRightCategoryTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SharedPasswordHasRightNamesTest()
+        {
+
+        }
+    }
+}
