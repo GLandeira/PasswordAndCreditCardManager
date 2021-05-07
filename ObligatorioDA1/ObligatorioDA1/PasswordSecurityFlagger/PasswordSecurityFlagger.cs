7de1074a01006a94cd71d4d2bda10d04a316640b
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-05-07 16:24:37 -0300

    Fixed PasswordSecurityFlagger namespace

--- ObligatorioDA1/ObligatorioDA1/PasswordSecurityFlagger/PasswordSecurityFlagger.cs
+++ ObligatorioDA1/ObligatorioDA1/PasswordSecurityFlagger/PasswordSecurityFlagger.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Domain.Exceptions;
 
-namespace Domain
+namespace Domain.PasswordSecurityFlagger
 {
     public class PasswordSecurityFlagger
     {
