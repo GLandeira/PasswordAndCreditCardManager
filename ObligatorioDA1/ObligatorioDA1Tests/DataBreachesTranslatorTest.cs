fa6cdc3b7a7b1024aa09066e01d7125817c132b4
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-05-12 20:59:44 -0300

    Add tests for Translator.

---
+++ ObligatorioDA1/ObligatorioDA1Tests/DataBreachesTranslatorTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+
+namespace DomainTests
+{
+    /// <summary>
+    /// Summary description for DataBreachesTranslatorTest
+    /// </summary>
+    [TestClass]
+    public class DataBreachesTranslatorTest
+    {
+        public DataBreachesTranslatorTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
