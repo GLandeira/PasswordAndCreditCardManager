904f40c03a4a62219b3ffa8638ffe9b053d91046
GLandeira <gastonlandeira@gmail.com>
2021-05-12 22:59:09 -0300

    Revert "Same but with sitePasswordException"
    This reverts commit 84e11323cccb57bc744b7ca9436b3d288b6d39ec.

---
+++ ObligatorioDA1/ObligatorioDA1/Exceptions/SitePasswordException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+
+    public class SitePasswordException : PasswordExceptions
+
+    {
+        public SitePasswordException()
+        {
+        }
+
+        public SitePasswordException(string message) : base(message)
+        {
+        }
+
+        public SitePasswordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public override string Message => "Site needs to be between 3 and 25 characters";
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
