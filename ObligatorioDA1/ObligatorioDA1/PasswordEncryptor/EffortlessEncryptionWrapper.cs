db4b39fb4de942e16391b008e656c0be54d9a427
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-06-02 17:21:15 -0300

    Added implementation that satisfies red tests.

--- ObligatorioDA1/ObligatorioDA1/PasswordEncryptor/EffortlessEncryptionWrapper.cs
+++ ObligatorioDA1/ObligatorioDA1/PasswordEncryptor/EffortlessEncryptionWrapper.cs
@@ -9,14 +9,26 @@ namespace Domain.PasswordEncryptor
 {
     public class EffortlessEncryptionWrapper : IEncryptor
     {
+        private const string SALT = "saldemesa";
+        private byte[] _iv;
+        private byte[] _key;
+
         public string Encrypt(string password)
         {
-            return password;
+            _iv = Bytes.GenerateIV();
+            _key = Bytes.GenerateKey();
+
+            string encryptedPassword = Strings.Encrypt(SALT + password, _key, _iv);
+
+            return encryptedPassword;
         }
 
         public string Decrypt(string encryptedPassword)
         {
-            return encryptedPassword;
+            string decryptedPassword = Strings.Decrypt(encryptedPassword, _key, _iv);
+            string saltlessPassword = decryptedPassword.Substring(SALT.Length, decryptedPassword.Length - SALT.Length);
+
+            return saltlessPassword;
         }
     }
 }
