c8ae437746ec03707db29c32ed2ae21cfaf48a6e
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-06-03 16:28:27 -0300

    Created DataAccess Interface.

---
+++ ObligatorioDA1/Repository/IDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IDataAccess<T>
+    {
+        void Add(T entity);
+        void Modify(T entity);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Delete(T entity);
+    }
+}
