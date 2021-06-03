4efea32454386d31be59bbf9ecd80994a5c30230
Matias González <matias.gonzalez.2407@gmail.com>
2021-06-03 16:08:26 -0300

    Database first migration

---
+++ ObligatorioDA1/Repository/DomainDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Repository
+{
+    public class DomainDBContext : DbContext
+    {
+        public DbSet<CreditCard> CreditCards { get; set;}
+        
+        public DomainDBContext() : base("name=DomainDBContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CreditCard>().HasKey(c => c.Number);
+        }
+    }
+}
