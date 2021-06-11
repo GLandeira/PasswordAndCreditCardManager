60634fd0b27b2e8aa158d38c745fa77542e2b40a
GLandeira <gastonlandeira@gmail.com>
2021-06-11 16:28:00 -0300

    Migration added

---
+++ ObligatorioDA1/Repository/Migrations/202106111919196_PasswordCompleteCascade.cs
@@ -0,0 +1,26 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PasswordCompleteCascade : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Passwords", "UserPassword_UserPasswordID", "dbo.UserPasswords");
+            DropIndex("dbo.Passwords", new[] { "UserPassword_UserPasswordID" });
+            AlterColumn("dbo.Passwords", "UserPassword_UserPasswordID", c => c.Int(nullable: false));
+            CreateIndex("dbo.Passwords", "UserPassword_UserPasswordID");
+            AddForeignKey("dbo.Passwords", "UserPassword_UserPasswordID", "dbo.UserPasswords", "UserPasswordID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Passwords", "UserPassword_UserPasswordID", "dbo.UserPasswords");
+            DropIndex("dbo.Passwords", new[] { "UserPassword_UserPasswordID" });
+            AlterColumn("dbo.Passwords", "UserPassword_UserPasswordID", c => c.Int());
+            CreateIndex("dbo.Passwords", "UserPassword_UserPasswordID");
+            AddForeignKey("dbo.Passwords", "UserPassword_UserPasswordID", "dbo.UserPasswords", "UserPasswordID");
+        }
+    }
+}
