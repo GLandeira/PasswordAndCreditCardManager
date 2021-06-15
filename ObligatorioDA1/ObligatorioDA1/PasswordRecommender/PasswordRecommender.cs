808df3fc6eea1cc4d4b49fec43b620510800f22b
Matias González <matias.gonzalez.2407@gmail.com>
2021-06-15 12:25:12 -0300

    Implemented PasswordRecommender

--- ObligatorioDA1/ObligatorioDA1/PasswordRecommender/PasswordRecommender.cs
+++ ObligatorioDA1/ObligatorioDA1/PasswordRecommender/PasswordRecommender.cs
@@ -9,6 +9,40 @@ namespace Domain.PasswordRecommender
 {
     public class PasswordRecommender
     {
-        
+        public static bool isASafePassword(string passwordString, User loggedUser)
+        {
+            bool isSafe = true;
+
+            //Check if its in data breaches
+            foreach (DataBreach db in loggedUser.UserDataBreaches.DataBreaches)
+            {
+                foreach (PasswordHistory ph in db.PasswordBreaches)
+                {
+                    if (ph.BreachedPasswordString == passwordString)
+                    {
+                        isSafe = false;
+                    }
+                }
+            }
+
+            //Check if its used already
+            foreach (Password p in loggedUser.UserPasswords.Passwords)
+            {
+                if (p.PasswordString == passwordString)
+                {
+                    isSafe = false;
+                }
+            }
+
+
+            //Check password level
+            SecurityLevelPasswords passwordLevel = PasswordSecurityFlagger.PasswordSecurityFlagger.GetSecurityLevel(passwordString);
+            if (!(passwordLevel == SecurityLevelPasswords.DARK_GREEN || passwordLevel == SecurityLevelPasswords.LIGHT_GREEN))
+            {
+                isSafe = false;
+            }
+
+            return isSafe;
+        }
     }
 }
