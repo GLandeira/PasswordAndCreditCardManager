4d089278b1690a8a87cc2642fdcf8afbff350513
Inaki Etchegaray <42726328+SleepyWolfy@users.noreply.github.com>
2021-06-16 18:20:59 -0300

    Added migration for passwords to have Key and IV

---
+++ ObligatorioDA1/Repository/Migrations/202106162119429_AddedPasswordKeyAndIV.cs
@@ -0,0 +1,20 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPasswordKeyAndIV : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Passwords", "PasswordIV", c => c.String());
+            AddColumn("dbo.Passwords", "PasswordKey", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Passwords", "PasswordKey");
+            DropColumn("dbo.Passwords", "PasswordIV");
+        }
+    }
+}
